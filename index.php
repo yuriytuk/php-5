4bcc3952617751315e1c3a5dca0054f41f0fa3fa
yuriytuk <yuriytukal@yandex.ru>
2025-09-06 10:30:39 +0300

    исправления

--- index.php
+++ index.php
@@ -3,28 +3,30 @@
     function daysWork($year, $month) {
         $daysMonth = cal_days_in_month(CAL_GREGORIAN, $month, $year);
         // Первое число месяца - это первый рабочий день.
-        $workDays = 1;
+        $workDays = 0;
+        $workDaysMonth = "";
         
         for ($day = 1; $day <= $daysMonth; $day++) {
             $date = "$year-$month-$day";
             $dayOfWeek = date('N', strtotime($date));
             
             if ($workDays == 1) {
-                echo ($date . " + " . PHP_EOL);
+                $workDaysMonth = $workDaysMonth . ($date . " + " . PHP_EOL);
                 $workDays++;
                 $day += 2;
                 continue;
             }
             if ($dayOfWeek <= 5) {
-                echo ($date . " + " . PHP_EOL);
+                $workDaysMonth = $workDaysMonth . ($date . " + " . PHP_EOL);
                 $workDays++;
                 $day += 2;
             } else {
-                echo ($date . PHP_EOL);
+                $workDaysMonth = $workDaysMonth . ($date . PHP_EOL);
             }
         }
+        echo $workDaysMonth;
     }
 
     $year = 2025;
-    $month = 9;
+    $month = 8;
     $workingDays = daysWork($year, $month);
